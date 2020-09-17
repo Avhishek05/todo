62d5711cf8916075f409e99fb4f0f0620b815973
Abhishek Yadav <avhishekydv@gmail.com>
2020-09-18 02:19:07 +0530

    Updates

---
+++ precache-manifest.9a7e08039f43615dd0652f77334900e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba14e3a4553a3a088830e05891c0fb83",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "ed685fadc651e566dd0c",
+    "url": "/todo/static/css/main.8ce52009.chunk.css"
+  },
+  {
+    "revision": "345d537b81e44290a6f2",
+    "url": "/todo/static/js/2.8c95b0b2.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/todo/static/js/2.8c95b0b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed685fadc651e566dd0c",
+    "url": "/todo/static/js/main.484aa18a.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
